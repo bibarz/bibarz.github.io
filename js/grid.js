012472f1b4b332bb6306e6bfeb91703e16dcecf2
c_borjag <ibarz@braincorporation.com>
2014-04-13 01:43:45 -0700

    update

--- js/grid.js
+++ js/grid.js
@@ -7,12 +7,12 @@ function Grid(size, previousState, value_jpg_refs) {
   }
   else {
       this.value_jpg_refs = {}
-      this.value_jpg_refs[2] = 2 + Math.floor(Math.random() * 2);
+      this.value_jpg_refs[2] = 0 + Math.floor(Math.random() * 4);
       this.value_jpg_refs[4] = 4 + Math.floor(Math.random() * 3);
       this.value_jpg_refs[8] = 8 + Math.floor(Math.random() * 2);
       this.value_jpg_refs[16] = 16 + Math.floor(Math.random() * 2);
       this.value_jpg_refs[32] = 32;
-      this.value_jpg_refs[64] = 64 + Math.floor(Math.random() * 2);
+      this.value_jpg_refs[64] = 64 + Math.floor(Math.random() * 3);
       this.value_jpg_refs[128] = 128;
       this.value_jpg_refs[256] = 256 + Math.floor(Math.random() * 2);
       this.value_jpg_refs[512] = 512;
