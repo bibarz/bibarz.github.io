2b46cd2aee1dda1c6829b33e38b75209849884fd
c_borjag <ibarz@braincorporation.com>
2014-04-20 17:22:21 -0700

    wait

--- js/game_manager.js
+++ js/game_manager.js
@@ -101,7 +101,8 @@ GameManager.prototype.load_source = function(sound_source) {
 
 GameManager.prototype.playSound = function(sound_source) {
     if (!(sound_source in this.sounds)) this.load_source(sound_source);
-    this.sounds[sound_source].play();
+    var s = this.sounds[sound_source];
+    setTimeout(function () {s.play();}, 50);
 };
 
 GameManager.prototype.addRandomTile = function () {
