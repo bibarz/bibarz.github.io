1592c3fcec6a00235fa631271fe9a50b77c91f79
c_borjag <ibarz@braincorporation.com>
2014-04-24 17:08:42 -0700

    err

--- js/analytics.js
+++ js/analytics.js
@@ -3,5 +3,5 @@
  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-50387609-1', 'noventayochoalaoctava.github.io');
+ga('create', 'UA-50387609-2', 'bibarz.github.io');
 ga('send', 'pageview');
